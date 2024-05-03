2de32fc856d855cd6fe341a467d94aeae173ad30
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-05-03 17:40:23 +0530

    done with message api and got some issue with this

--- src/app/api/accept-messages/route.ts
+++ src/app/api/accept-messages/route.ts
@@ -54,3 +54,46 @@ export async function POST(request: Request){
     }
 
 }
+
+export async function GET(request: Request){
+    await dbConnect();
+
+    const session =await getServerSession(authOptions)
+    const user: User =session?.user as User;
+
+        if(!session || !session.user){
+            return Response.json({
+                success: false,
+                message: 'NOt Authenticated',
+            },
+        { status: 401 }
+      )
+    }
+    const userId =user._id;
+    try {
+        const foundUser = await UserModel.findById(userId)
+        if(!foundUser){
+                return Response.json({
+                    success: false,
+                    message: 'User not found',
+                },
+                {status:404}
+            )
+        }
+        return Response.json({
+            success: true,
+            isAcceptingMessage:foundUser.isAcceptingMessage ,
+        },
+        {status:200}
+    )
+    } catch (error) {
+        console.log("failed to update status to accept messages");
+
+        return Response.json({
+            success: false,
+            message: 'Error is getting message acceptance status',
+        },
+        {status:500}
+    )
+    }
+}
