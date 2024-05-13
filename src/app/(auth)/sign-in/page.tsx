b0ed5f40102a6f9b793794a76ed08c9e231fdbe4
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-05-13 08:34:13 +0530

    done with signin and made route for dashboard and message page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -15,11 +15,12 @@ import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, For
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { Loader2 } from "lucide-react"
+import { signIn } from "next-auth/react"
+import { signInSchema } from "@/schemas/signInSchema"
 
 
 const page = () => {
   
-  const [isSubmitting,setIsSubmitting] =useState(false)
 
   const {toast} =useToast()
   const router =useRouter()
@@ -28,36 +29,27 @@ const page = () => {
   const form =useForm<z.infer<typeof signInSchema>>({
     resolver: zodResolver(signInSchema),
     defaultValues:{
-      username:'',
-      email:'',
+      identifier:'',
       password:''
     }
   })
 
   
-  const onSubmit = async (data: z.infer<typeof signUpSchema>) =>{
-    setIsSubmitting(true)
-    try {
-      const response = await axios.post<ApiResponse>('/ap1/sign-up',data)
+  const onSubmit =async (data:z.infer<typeof signInSchema>) => {
+    const result = await signIn('credentials',{
+      redirect:false,
+      identifier:data.identifier,
+      password:data.password
+    })
+    if(result?.error){
       toast({
-        title:'Success',
-        description:response.data.message,
-      
+        title:"Login Failed",
+        description:"Incorrect username or password",
+        variant:"destructive"
       })
-      router.replace(`/verify/${username}`)
-      setIsSubmitting(false)
-    } catch (error) {
-      console.log("Error in signup of user", error);
-      const axiosError = error as AxiosError<ApiResponse>;
-      let errorMessage = axiosError.response?.data.message
-      toast({
-        title:'Signup failed',
-        description:errorMessage,
-        variant: "destructive"
-       
-      })
-      setIsSubmitting(false)
-
+    }
+    if (result?.url) {
+      router.replace('/dashboard')
     }
   }
 
@@ -71,47 +63,21 @@ const page = () => {
           tracking-tight lg:text-5xl mb-6">
             Join Mystery Message
           </h1>
-          <p className="mb-4">Sign up to start your anonymous adventure</p>
+          <p className="mb-4">Sign in to start your anonymous adventure</p>
           
         </div>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)}
            className="space-y-6">
-            <FormField
-              name="username"
-              control={form.control}
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Username</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Username"
-                     {...field}
-                     onChange={(e)=>{
-                      field.onChange(e)
-                      debounced(e.target.value)
-                     }}
-                      />
-                      
-                  </FormControl>
-                     {isCheckingUsername && <Loader2 className="animate-spin"/>}
-                     <p className={`text-sm ${usernameMessage ==="Username is unique" ?'text-green-500':'text-red-600'}`}>
-                        test {usernameMessage}
-                     </p>
-                  <FormDescription>
-                    This is your public display name.
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-                )}
-             />
+
              <FormField
-              name="email"
+              name="identifier"
               control={form.control}
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Email</FormLabel>
+                  <FormLabel>Email/Username</FormLabel>
                   <FormControl>
-                    <Input placeholder="email"
+                    <Input placeholder="email/username"
                      {...field}
                       />
                   </FormControl>
@@ -135,14 +101,8 @@ const page = () => {
                 </FormItem>
                 )}
              />
-             <Button type="submit" disabled={isSubmitting}>
-              {
-                isSubmitting? (
-                  <>
-                  <Loader2 className="mr-2 h-4 w-4 animate-spin"/>Please wait
-                  </>
-                ) :( "Signup")
-              }
+             <Button type="submit" >
+              SignIn
              </Button>
            </form>
         </Form>
